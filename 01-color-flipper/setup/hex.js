a506e8348d82fe732bb4378713679fd2f8fee37e
Sebastien Charlet <scharlet.fr@gmail.com>
2021-12-16 11:34:55 +0100

    01 - "Hex" button implementation

--- 01-color-flipper/setup/hex.js
+++ 01-color-flipper/setup/hex.js
@@ -1,1 +1,20 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
+
+btn.addEventListener('click', function(){
+	let hexColor = '#';
+
+	for(let i = 0; i<6; i++){
+		hexColor+=hex[getRandomNumber()];
+	}
+	// get a random between 0-3
+	const randomNumber = getRandomNumber();
+	document.body.style.backgroundColor = hexColor;
+	color.textContent = hexColor;
+})
+
+function getRandomNumber(){
+    // generate random numbers within 0 to 3
+    return Math.floor(Math.random() * 16) ;
+}
